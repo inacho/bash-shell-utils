facbbd1eb69882c24f143286293ccd36d37e8ae1
Ignacio de Tomás <driews@gmail.com>
2013-09-04 11:38:01 +0200

    Updated examples

--- demo_functions.sh
+++ demo_functions.sh
@@ -4,13 +4,22 @@ source "functions/count_lines.sh"
 source "functions/format_size.sh"
 source "functions/format_time.sh"
 
+
 # count_lines
 echo "The file /etc/passwd has $(count_lines "/etc/passwd") lines"
+# Output example:
+#   The file /etc/passwd has 83 lines
+
 
 # format_size
 size=92873496
 echo "$size Bytes = $(format_size $size)"
+# Output example
+#   92873496 Bytes = 88,57 MiB
+
 
 # format_time
 seconds=86600
 echo "$seconds Seconds = $(format_time $seconds)"
+# Output example
+#   86600 Seconds = 1 day, 3 minutes, 20 seconds
